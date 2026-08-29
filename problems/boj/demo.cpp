ef5329ecbb42688c08e4277fce0f9e199f789c4a
『              』 <a01021286033@gmail.com>
2026-08-29 20:08:47 +0900

    Update settings.json

--- problems/boj/demo.cpp
+++ problems/boj/demo.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <optional>
 
 using namespace std;
 
+class ScoreTracker {
+private:
+    int total;
+    int count;
+
+public:
+    ScoreTracker();
+
+    void addScore(int score);
+    optional<double> average() const;
+    int size() const;
+    void clear();
+};
+
+
 int main() {
-    vector<double> v;
+
 
 }
+
